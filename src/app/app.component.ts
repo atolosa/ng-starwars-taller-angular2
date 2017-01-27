8540c5f1a8ee22a43ef93d90c4a8b3891aa06431
Ana Amelia Tolosa González (EPS) <wwwadm11@clL27-28>
2017-01-27 20:43:56 +0100

    viernes 27/01

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Http, Response} from "@angular/http";
+import "rxjs";
+import {SwapiService} from "./core/swapi.service";
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
   title = 'app works!';
+  people = [];
+
+  constructor(private swapiService:SwapiService){ //en el constructor lo usamos para inyección de dependencias
+
+  }
+
+  ngOnInit(){
+    // debugger; // por si queremos poner puntos de ruptura, se pone debugger
+    this.swapiService
+      .getPeopleList()
+      .subscribe((people) => this.people = people);
+
+  }
+
 }
